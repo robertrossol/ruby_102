03eae51452626ec4e216aa564ad839ca670d3323
Robert Rossol <rrossol88@gmail.com>
2022-02-01 13:50:59 -0600

    Updated rubocop filters and completed rock papaper scissors game

---
+++ lesson_2_OOP/RPS_Outline.rb
@@ -0,0 +1,28 @@
+# 1. Write a textual description of the problem or exercise.
+# 2. Extract the major nouns and verbs from the description.
+# 3. Organize and associate the verbs with the nouns.
+# 4. The nouns are the classes and the verbs are the behaviors or methods.
+
+# 1. 
+# Rock, Paper, Scissors is a two-player game where each player chooses
+# one of three possible moves: rock, paper, or scissors. The chosen moves
+# will then be compared to see who wins, according to the following rules:
+
+# - rock beats scissors
+# - scissors beats paper
+# - paper beats rock
+
+# If the players chose the same move, then it's a tie.
+
+# 2. 
+# Nouns: player, move, rule
+# Verbs: choose, compare
+
+# 3.
+# Player
+#  - choose
+# Move
+# Rule
+
+# - compare
+# it's not obvious where the "compare" verb goes. 
